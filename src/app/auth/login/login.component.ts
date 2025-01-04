17242a124d9178cdf7bd58e7aa2a673ee8d9036d
branka0906 <branka.krcijevic.1995@gmail.com>
2025-01-04 16:13:12 +0100

    login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { NgForm } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [NgIf, FormsModule, MatButtonModule, MatFormFieldModule, MatInputModule, RouterLink, MatCardModule, MatToolbarModule, MatIconModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+    errorExists = false;
+    errorText="";
 
+    constructor(private router: Router){}
+
+    onSubmit(form: NgForm){
+
+    }
+    
 }
